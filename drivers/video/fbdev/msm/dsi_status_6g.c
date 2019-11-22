8be54080325039542e94e97a423aad6e313667d3
ZyCromerZ <neetroid97@gmail.com>
2019-11-23 05:30:28 +0700

    fix some bugs
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- drivers/video/fbdev/msm/dsi_status_6g.c
+++ drivers/video/fbdev/msm/dsi_status_6g.c
@@ -170,7 +170,8 @@ void mdss_check_dsi_ctrl_status(struct work_struct *work, uint32_t interval)
 
 	if (pstatus_data->mfd->panel_power_state == MDSS_PANEL_POWER_ON) {
 /* Huaqin duchangguo modify for disabling esd check when panel is not connect before boot start*/
-		if (ret > 0)
+		if (ret > 0) {
+			pstatus_data->is_first_check = 0;
 			schedule_delayed_work(&pstatus_data->check_status,
 				msecs_to_jiffies(interval));
 		}
